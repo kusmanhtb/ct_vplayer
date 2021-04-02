c6fde3851836e84785e0018853d9e4f1a24fc4e4
Ramakrishnan M <60294235+Raamkee@users.noreply.github.com>
2021-04-02 12:31:36 +0530

    Blank App
    Blank App

---
+++ android/app/src/main/kotlin/com/example/ct_vplayer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ct_vplayer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=G:\Ramakrishnan\Softwares\flutter_windows_1.20.2-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=G:\Ramakrishnan\Project Files\Flutter\ct_vplayer"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
